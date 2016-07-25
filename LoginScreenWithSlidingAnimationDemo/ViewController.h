60f1a9bd77c13acf31ae1980f7fc62441d011606
WebsoftProfession <dotsquares@deepaksharma2.local>
2016-07-25 10:52:02 +0530

    Initial Commit

---
+++ LoginScreenWithSlidingAnimationDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LoginScreenWithSlidingAnimationDemo
+//
+//  Created by Dotsquares on 7/25/16.
+//  Copyright © 2016 Dotsquares. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ LoginScreenWithSlidingAnimationDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LoginScreenWithSlidingAnimationDemo
+//
+//  Created by Dotsquares on 7/25/16.
+//  Copyright © 2016 Dotsquares. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
